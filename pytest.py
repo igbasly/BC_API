ba65d29b04d9d7e10cc70bc77f8d64115ba6264d
ianglbas <igbasly@uc.cl>
2019-07-19 22:22:31 -0400

    Travis CI update :ok_hand:

--- pytest.py
+++ pytest.py
@@ -5,20 +5,20 @@ from json import load, loads
 with open("tests.json", "r") as file:
     TESTS = load(file)
 
-url = "/api/v1?"
+url_1 = "/api/v1?"
 print("\n---- Starting Tests ----\n")
 for test in TESTS:
     print(f"Running {test}")
     data = TESTS[test]
-    req = data[0]
-    res = data[1]
+    url = data[0]
+    req = data[1]
+    res = data[2]
     url_test = url + "&".join(
         [f"{t[0]}={'+'.join(t[1].split(' '))}" for t in req])
     with app.test_client() as C:
         resp_test = C.get(url_test)
         for r in req:
             assert request.args[r[0]] == r[1]
-        #print(resp_test.get_json())
         assert resp_test.get_json() == res
 
 print("\n---- Tests finished successfully ----\n")
