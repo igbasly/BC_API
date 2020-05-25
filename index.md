2569ecc01d2c0ebe2cd685ba260508a0e1d0ecb6
Ian Basly <igbasly@uc.cl>
2020-05-25 01:36:57 -0400

    Documentation update

--- index.md
+++ index.md
@@ -1,4 +1,4 @@
-# BuscaCursos UC REST API  | [![Build Status](https://travis-ci.com/igbasly/BC_API.svg?token=rvsCi5nQd3Zv6KdSdS54&branch=master)](https://travis-ci.com/igbasly/BC_API)
+# BuscaCursos UC REST API  | [![Build Status](https://travis-ci.com/igbasly/BC_API.svg?token=rvsCi5nQd3Zv6KdSdS54&branch=master)](https://travis-ci.com/igbasly/BC_API) [![Heroku](https://heroku-badge.herokuapp.com/?app=buscacursos-api&root=favicon.ico)]()
 REST API del sistema BuscaCursos de la PUC Chile.
 
 La API cuenta con 3 versiones:
---
+++ scripts.js
@@ -0,0 +1,46 @@
+
+function checkCheckbox() {
+    const check1 = document.getElementById("vac");
+    const check2 = document.getElementById("req");
+    if (!check1.checked){
+        check1.value = "";
+    } else{
+        check1.value = "true";
+    };
+    if (!check2.checked){
+        check2.value = "";
+    } else{
+        check2.value = "true";
+    };
+}
+
+async function callAPI() {
+    checkCheckbox();
+    let form = document.getElementById("form");
+    let query = [];
+    let response = {};
+    let semester = "";
+    for (let i = 0; i < form.length; i++) {
+        const element = form.elements[i];
+        if (element.type != "submit" && element.name != "semestre") {
+            if (!(element.value == "" || element.value == "TODOS")){
+                query.push(element.name + "=" + element.value)
+            };
+        } else if (element.name == "semestre"){
+            semester = "semestre=" + element.value;
+        };
+    };
+    if (query.length > 0){
+        query.unshift(semester);
+        query = 'http://buscacursos-api.herokuapp.com/api/v3?' + query.join("&");
+        console.log(query)
+        let resp = await fetch(query);
+        response = await resp.json();
+        console.log(response)
+    } else {
+        console.log("No query")
+        query = "http://buscacursos-api.herokuapp.com/api/v3?";
+    };
+    response = JSON.stringify(response, null, 4);
+    return [query, response];
+}
