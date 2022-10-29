ed4ed368b0e1deadd86a44d1fe06d36410ac14a1
Ian <igbasly@uc.cl>
2022-10-29 17:43:21 -0300

    feat(app) add basic app info

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,10 @@ from fastapi import FastAPI
 from .config import MainRouter
 
 
-app = FastAPI()
+app = FastAPI(
+    title="BuscaCursos UC API",
+    description="REST API para obtener información desde BuscaCursos UC, \
+        creada para el funcionamiento de https://www.horariomaker.com.",
+    version="4.0.0",
+)
 app.include_router(MainRouter)
