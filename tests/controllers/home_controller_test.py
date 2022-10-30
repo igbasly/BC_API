5d2e4adbe4fc7fe6ea4097db892f2812b174bb19
Ian <igbasly@uc.cl>
2022-10-29 21:07:45 -0300

    test(home-controller): fix text for change on documentation

--- tests/controllers/home_controller_test.py
+++ tests/controllers/home_controller_test.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_index():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.url == client.base_url + "/redoc"
+    assert response.url == client.base_url + "/documentation"
